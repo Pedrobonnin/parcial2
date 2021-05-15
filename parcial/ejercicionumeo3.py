c2d16d09b8409233b5ab7d1b73e11845fd6dafca
Pedrobonnin <80923995+Pedrobonnin@users.noreply.github.com>
2021-05-14 22:19:24 -0300

    Add files via upload

--- parcial/ejercicionumeo3.py
+++ parcial/ejercicionumeo3.py
@@ -1,20 +1,3 @@
-# from random import randint
-# lista = []
-
-# for i in range (0,78):
-#     nummero1 = randint(1,134000)
-#     lista.append(nummero1)
-
-# lista.sort()
-
-# print ("El menor es ",lista[0])
-# print ("El mayor es ",lista[77])
-
-# print(lista)
-# for lista in range (0,78):
-#     if (lista % 2==0):
-#         print(lista)
-
 from random import randint
 lista = []
 
